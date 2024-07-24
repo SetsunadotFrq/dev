14fe694b9b5b9b080419b585a3e826078cd42320
Setsuna <149562079+SetsunadotFrq@users.noreply.github.com>
2024-07-24 09:06:50 +0900

    uploadapi.vb を更新

---
+++ uploadapi.vb
@@ -0,0 +1,40 @@
+Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Net.Http
+Imports System.Threading.Tasks
+Imports System.Web.Http
+
+Namespace YourNamespace
+    Public Class UploadController
+        Inherits ApiController
+
+        <HttpPost>
+        <Route("api/upload")>
+        Public Async Function UploadFile() As Task(Of HttpResponseMessage)
+            If Not Request.Content.IsMimeMultipartContent() Then
+                Return Request.CreateResponse(HttpStatusCode.UnsupportedMediaType)
+            End If
+
+            Try
+                Dim streamProvider = New MultipartMemoryStreamProvider()
+                Await Request.Content.ReadAsMultipartAsync(streamProvider)
+
+                For Each file In streamProvider.Contents
+                    Dim filename = file.Headers.ContentDisposition.FileName.Trim(""""c)
+                    Dim buffer = Await file.ReadAsByteArrayAsync()
+                    Dim filePath = Path.Combine("C:\Uploads", filename)
+
+                    ' ファイルを書き込むためにファイルストリームを使用
+                    Using fileStream As New FileStream(filePath, FileMode.Create, FileAccess.Write)
+                        fileStream.Write(buffer, 0, buffer.Length)
+                    End Using
+                Next
+
+                Return Request.CreateResponse(HttpStatusCode.OK)
+            Catch ex As Exception
+                Return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message)
+            End Try
+        End Function
+    End Class
+End Namespace
